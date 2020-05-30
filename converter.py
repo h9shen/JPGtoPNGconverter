74e90e9517f50f1ad9cc8a92e6068a4e04acb2d6
Haotian Shen <haotianshen@Frank-2.local>
2020-05-30 01:23:10 -0400

    jpg to png project

---
+++ converter.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageFilter
+import sys
+import os
+
+# Grab the input and output folder
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# Check if output folder exists, if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}', 'png')
+
+print('all done!')
